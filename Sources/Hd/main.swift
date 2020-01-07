f4f0077411bb87d9ac4fa683632ef0baf51973e0
Bill Donner <billdonner@gmail.com>
2020-01-07 13:49:26 -0500

    just main

--- Sources/Hd/main.swift
+++ Sources/Hd/main.swift
@@ -6,114 +6,142 @@ import Plot
 struct Hd: Website {
     enum SectionID: String, WebsiteSectionID {
         // Add the sections that you want your website to contain here:
+        
+        case about
         case posts
+        case links
     }
-
+    
     struct ItemMetadata: WebsiteItemMetadata {
         // Add any site-specific metadata that you want to use here.
     }
-
+    
     // Update these properties to configure your website:
     var url = URL(string: "http://shtickerz.com")!
-    var name = "Hd"
+    var name = "About Half Dead"
     var description = "The Greatest Band In A Very Small Land"
     var language: Language { .english }
-    var imagePath: Path? { nil }
+    var imagePath: Path? { "https://billdonner.com/images/austinmay2019.jpg" }
 }
 
+extension PublishingStep where Site == Hd {
+    static func addDefaultSectionTitles() -> Self {
+        .step(named: "Default section titles") { context in
+            context.mutateAllSections { section in
+                guard section.title.isEmpty else { return }
 
-//import SPMCrawlingCore
-
-func printUsage() {
-    let processinfo = ProcessInfo()
-    print(processinfo.processName)
-    let path = LibraryDirectoryURL.absoluteString
-    let nam = FileManager.default.displayName(atPath: path)
-    print(nam)
-    
-    // Get display name, version and build
-    
-    if let displayName =
-        Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String{
-        print(displayName)
+                switch section.id {
+                case .posts:
+                    section.title = "Audio"
+                case .links:
+                    section.title = "External links"
+                case .about:
+                    section.title = "About Half Ded"
+                }
+            }
+        }
     }
-    if let version =
-        Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String{
-        print(version)
-    }
-    if let build =
-        Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as?  String {
-        print(build)
-    }
-    if let bundleID  =
-        Bundle.main.infoDictionary?[ "CFBundleIdentifier"] as? String {
-        print(bundleID)
-    }
-    let executableName = (CommandLine.arguments[0] as NSString).lastPathComponent
-    
-    print("\(executableName) 1.0.3 ")
-    print("usage:")
-    print("\(executableName) -j config-file-url json-output-file [base-url]")
-    print("or")
-    print("\(executableName) -c config-file-url csv-output-file [base-url]")
-    print("or")
-    print("\(executableName) -jv or -cv for vebose output")
-}
-func exitBadCommand() {
-    print("""
-        {"invalid-command":\(CommandLine.arguments), "status":401}
-        """)
-    printUsage()
-    
 }
 
-
-private var verbosity:Bool = false
-private var baseURL:URL? = nil
-private var opath:String!
-
-// -json and -csv go to adforum for now, -text goes to manifezz
-
-do {
-    guard CommandLine.arguments.count > 4 else { exitBadCommand(); exit(0)  }
-    let arg0 = CommandLine.arguments[1].dropFirst()
-    let subargs = arg0.components(separatedBy: ",")
+func command_main() {
     
-    if  subargs.count>1 {
-        verbosity  = subargs[1].hasPrefix("v")
-    } else { verbosity = false }
-    if CommandLine.arguments.count > 4  {
-        guard let b =  URL(string: CommandLine.arguments[4]) else {exitBadCommand(); exit(0)  }
-        baseURL =   b
+    func printUsage() {
+        let processinfo = ProcessInfo()
+        print(processinfo.processName)
+        let path = LibraryDirectoryURL.absoluteString
+        let nam = FileManager.default.displayName(atPath: path)
+        print(nam)
+        
+        // Get display name, version and build
+        
+        if let displayName =
+            Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String{
+            print(displayName)
+        }
+        if let version =
+            Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String{
+            print(version)
+        }
+        if let build =
+            Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as?  String {
+            print(build)
+        }
+        if let bundleID  =
+            Bundle.main.infoDictionary?[ "CFBundleIdentifier"] as? String {
+            print(bundleID)
+        }
+        let executableName = (CommandLine.arguments[0] as NSString).lastPathComponent
+        
+        print("\(executableName) 1.0.3 ")
+        print("usage:")
+        print("\(executableName) -j config-file-url json-output-file [base-url]")
+        print("or")
+        print("\(executableName) -c config-file-url csv-output-file [base-url]")
+        print("or")
+        print("\(executableName) -jv or -cv for vebose output")
     }
-    opath =  (CommandLine.arguments[3])
-    guard let configurl = URL(string:CommandLine.arguments[2]) else  { exitBadCommand(); exit(0)  }
+    func exitBadCommand() {
+        print("""
+            {"invalid-command":\(CommandLine.arguments), "status":401}
+            """)
+        printUsage()
+        
+    }
     
-    let options:CrawlOptions  = verbosity ? CrawlOptions.verbose :  CrawlOptions.none
+    
+     var verbosity:Bool = false
+   var baseURL:URL? = nil
+    var opath:String!
+    
+    // -json and -csv go to adforum for now, -text goes to manifezz
     
     do {
-        try  ManifezzClass().bootCrawlMeister (name: opath.components(separatedBy: ".-").first ?? opath, baseURL:baseURL!,configURL:configurl,opath:opath,options:options,whenDone: {crawlResults in
-            print("[crawler] scanned \(crawlResults.count1) pages,  \(crawlResults.secsPerCycle*1000) ms per page")
-            // at this point we've plunked files into the designated directory
-            
-
-            // This will generate your website using the built-in Foundation theme:
-             try! Hd().publish(withTheme: .foundation)
-            
-//            try! Hd().publish(using: [
-//                .addMarkdownFiles(),
-//                .copyResources(),
-//               // .addFavoriteItems(),
-//               // .addDefaultSectionTitles(),
-//                .generateHTML(withTheme: .foundation)
-//                //.generateRSSFeed(including: [.recipes]),
-//               // .generateSiteMap()
-//            ])
-        })
-    }
-    catch {
-        exitBadCommand()
+        guard CommandLine.arguments.count > 4 else { exitBadCommand(); exit(0)  }
+        let arg0 = CommandLine.arguments[1].dropFirst()
+        let subargs = arg0.components(separatedBy: ",")
+        
+        if  subargs.count>1 {
+            verbosity  = subargs[1].hasPrefix("v")
+        } else { verbosity = false }
+        if CommandLine.arguments.count > 4  {
+            guard let b =  URL(string: CommandLine.arguments[4]) else {exitBadCommand(); exit(0)  }
+            baseURL =   b
+        }
+        opath =  (CommandLine.arguments[3])
+        guard let configurl = URL(string:CommandLine.arguments[2]) else  { exitBadCommand(); exit(0)  }
+        
+        let options:CrawlOptions  = verbosity ? CrawlOptions.verbose :  CrawlOptions.none
+        
+        do {
+            try  ManifezzClass().bootCrawlMeister (name: opath.components(separatedBy: ".-").first ?? opath, baseURL:baseURL!,configURL:configurl,opath:opath,options:options,whenDone: {crawlResults in
+                print("[crawler] scanned \(crawlResults.count1) pages,  \(crawlResults.secsPerCycle*1000) ms per page")
+                // at this point we've plunked files into the designated directory
+                
+                
+                // This will generate your website using the built-in Foundation theme:
+                try! Hd().publish(withTheme: .foundation)
+                
+                //            try! Hd().publish(using: [
+                //                .addMarkdownFiles(),
+                //                .copyResources(),
+                //               // .addFavoriteItems(),
+                //               // .addDefaultSectionTitles(),
+                //                .generateHTML(withTheme: .foundation)
+                //                //.generateRSSFeed(including: [.recipes]),
+                //               // .generateSiteMap()
+                //            ])
+            })
+        }
+        catch {
+            exitBadCommand()
+        }
     }
 }
+
+// really starts here
+
+command_main()
+
+
 
 
